91ba3446615837cab954b1ddfb08a88670c37084
kamil_wronka <113593650+kwronka@users.noreply.github.com>
2025-05-11 12:15:59 +0200

    Update main.c
    dodanie wczytywanie statystyk i dodawanie nowego gracza do statystyk

--- Tic-tac-toe/src/main.c
+++ Tic-tac-toe/src/main.c
@@ -42,12 +42,21 @@
         printf("Podaj nick gracza 1: ");
         fgets(player1.name,50, stdin);
         player1.name[strcspn(player1.name, "\n")] = 0;
+        if (!findPlayer(filename,player1.name,&player1)) {
+            player1.win=player1.lose=player1.draw=0;
+            addPlayer(filename,&player1);
+        }
+
         // tutaj jakoś pobranie statystk z pliku
 
         printf("Podaj nick gracza 2: ");
         fgets(player2.name,50, stdin);
         player2.name[strcspn(player2.name, "\n")] = 0;
-        //tez pobranie statystyk
+        if (!findPlayer(filename,player2.name,&player2)) {
+            player2.win=player2.lose=player2.draw=0;
+            addPlayer(filename,&player2);
+        }
+
 
         //game(&player1, &player2); // rozpoczynamy właściową grę
 
@@ -56,7 +65,7 @@
 
 
     }
-    // void game(Player player1,Player player2) {
+    // void game(Player *player1,Player *player2) {
 
     // }
     void printBoard(char board[3][3]) { //tutaj niby c tworzy kopie boardu, chyba trzeba użyc wskaźnika, jak nie używamy wskaźnika to nie zmienimy oryginalnej tablicy
@@ -80,7 +89,7 @@
                 else if (board[0][j] != ' ' && 
                     board[0][j] == board[1][j] && 
                     board[1][j] == board[2][j]) {
-                        return 1; // win by column
+                        return 1; // wygrana w kolumnie
                     }
             }
         }//  wygrana przekatne
@@ -97,10 +106,47 @@
         for (int j = 0; j < 3; j++)
             if (board[i][j] == ' ')
                 return 0;
-    return 1;
+    return 1; }
+
+    int findPlayer(const char *filename, const char *szukany_nick, Player *player) {
+    FILE *plik = fopen(filename, "r");
+    if (!plik) {
+        perror("Nie można otworzyć pliku do odczytu");
+        return 0;
+    }
+
+    char linia[100];
+    while (fgets(linia,100, plik)) {
+        Player tmp;
+        if (sscanf(linia, "%s %d %d %d", tmp.name, &tmp.win, &tmp.lose, &tmp.draw) == 4) { //patrzymy czy gracz jest juz zapisany w pliku
+            if (strcmp(tmp.name, szukany_nick) == 0) {
+                *player = tmp;
+                fclose(plik);
+                return 1;
+            }
+        }
+    }
+
+    fclose(plik);
+    return 0;
 }
+
+void addPlayer(const char *filename,Player *player) {
+    FILE *plik = fopen(filename, "a"); // tryb "a" = append (dopisywanie)
+    if (!plik) {
+        perror("Nie można otworzyć pliku d");
+        return;
+    }
+
+    fprintf(plik, "%s %d %d %d\n", (*player).name, (*player).win, (*player).lose, (*player).draw); // dopisujemy do pliku
+    fclose(plik);
+}
+
     
 int main() {
     menu();
+
+
     return 0;
 }
+
